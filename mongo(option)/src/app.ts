c43409abb72e36c7686881ca24b65d1b0f290492
Dan Berezin <Bogdanlinee@gmail.com>
2023-09-12 09:11:57 +0300

    test commit

--- mongo(option)/src/app.ts
+++ mongo(option)/src/app.ts
@@ -1,23 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
-import session from 'express-session';
-import fileStore from 'session-file-store';
+
 import morgan from 'morgan';
 
 dotenv.config();
 
 const app = express();
 const port = 5000;
-
-const FileStore = fileStore(session);
-
-app.use(session({
-  store: new FileStore({}),
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: true,
-}));
 
 // app routes
 import tasksRouter from './routes/tasksRoutes';
