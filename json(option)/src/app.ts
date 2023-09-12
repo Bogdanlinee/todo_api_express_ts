5b5fb4b5424d09e60b94669338ab5db44600e40e
Dan Berezin <Bogdanlinee@gmail.com>
2023-09-12 09:10:17 +0300

    Changed the projects` folders

--- json(option)/src/app.ts
+++ json(option)/src/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import fs from 'fs/promises';
-import path, { basename, dirname } from 'path';
+import path from 'path';
 
 const app = express();
 const port = 5000;
